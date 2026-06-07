6202aab3a002b638cc5987bb5ead39459a7843b0
Durgadevi-2675 <sdurgadevi091@gmail.com>
2026-06-07 14:00:47 +0530

    Add files via upload

---
+++ frontend-20260607T052558Z-3-001/frontend/src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(
+      CommonModule,
+      ReactiveFormsModule,
+      FormsModule
+    )
+  ]
+};
